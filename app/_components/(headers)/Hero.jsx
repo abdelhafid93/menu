0e49bb201165104ad45dd1e0d9dcc4f0dd96d831
Abdelhafid <wissalinas6@gmail.com>
2026-07-22 18:32:52 +0100

    Add scroll animations to Contact section using Framer Motion

--- app/_components/(headers)/Hero.jsx
+++ app/_components/(headers)/Hero.jsx
@@ -1,5 +1,7 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import {  motion } from "framer-motion";
 
 export default function Hero() {
   return (
@@ -22,20 +24,32 @@ export default function Hero() {
       {/* Content */}
       <div className="relative z-10 max-w-4xl mx-auto px-6 text-center text-white">
 
-        <h1 className="mt-6 text-5xl font-bold leading-tight md:text-7xl">
+        <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+        className="mt-6 text-5xl font-bold leading-tight md:text-7xl">
           Savourez une cuisine
           <br />
           <span className="text-amber-400">
             authentique et raffinée
           </span>
-        </h1>
+        </motion.h1>
 
-        <p className="mx-auto mt-6 max-w-2xl text-lg text-gray-200 md:text-xl">
+        <motion.p
+        initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+        className="mx-auto mt-6 max-w-2xl text-lg text-gray-200 md:text-xl">
           Des ingrédients frais, des recettes préparées avec passion et une
           ambiance chaleureuse pour partager des moments inoubliables.
-        </p>
+        </motion.p>
 
-        <div className="mt-10 flex flex-col justify-center gap-4 sm:flex-row">
+        <motion.div
+        initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.7 , delay:0.3 }}
+        className="mt-10 flex flex-col justify-center gap-4 sm:flex-row">
           <Link
             href="#menu"
             className="rounded-full bg-amber-500 px-8 py-4 font-semibold transition hover:bg-amber-600"
@@ -51,7 +65,7 @@ export default function Hero() {
           >
             Réserver sur WhatsApp
           </a>
-        </div>
+        </motion.div>
 
       </div>
     </section>
